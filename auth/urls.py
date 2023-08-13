623574bb135cfae0a8e1aaf2dc6836cf5144c362
Michael Adawaren <gadocer54@gmail.com>
2023-08-13 08:01:06 +0100

    updated Jwt 	modified:   auth/__pycache__/settings.cpython-310.pyc 	modified:   auth/__pycache__/urls.cpython-310.pyc 	modified:   auth/settings.py 	modified:   auth/urls.py 	new file:   base/api/__pycache__/serializers.cpython-310.pyc 	modified:   base/api/__pycache__/urls.cpython-310.pyc 	modified:   base/api/__pycache__/views.cpython-310.pyc 	modified:   base/api/serializers.py 	modified:   base/api/urls.py 	modified:   base/api/views.py

--- auth/urls.py
+++ auth/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
+
 urlpatterns = [
     path('api/', include('base.api.urls')), 
     path('admin/', admin.site.urls),
--- base/api/views.py
+++ base/api/views.py
@@ -1,6 +1,16 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
+    
+
+
+
 
 @api_view(['GET'])
 def getRoutes(request):
