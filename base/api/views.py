7d0d4ebb2e6bce7d6374dd900c9113979e1e13dc
Michael Adawaren <gadocer54@gmail.com>
2023-08-14 21:26:52 +0100

    changes 	modified:   base/api/__pycache__/views.cpython-310.pyc 	new file:   base/api/models.py 	modified:   base/api/views.py

--- base/api/views.py
+++ base/api/views.py
@@ -3,11 +3,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from  rest_framework import generics
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
     
     
+
+# class BlogPeirView(generics.ListAPIView):
+#     serializer_class = 
+
+
 
 
 
