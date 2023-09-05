70a35fff8e0c5d5daf018737a926be828bd01dbe
Michael Adawaren <gadocer54@gmail.com>
2023-09-05 22:05:38 +0100

    commit add blog: 	modified:   base/__pycache__/admin.cpython-310.pyc 	modified:   base/__pycache__/models.cpython-310.pyc 	modified:   base/admin.py 	deleted:    base/api/models.py 	new file:   base/migrations/0001_initial.py 	new file:   base/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   base/models.py

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blogs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    title = models.CharField(blank=False, null=False, max_length=500)
+    text = models.CharField(blank=False, null=False, max_length=500)
+  
+    
+    def __init__(self) -> None:
+        return self.title
